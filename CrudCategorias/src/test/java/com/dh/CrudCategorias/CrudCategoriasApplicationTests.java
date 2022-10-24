aaa4ab93c6ab048b30f36b70b4394909a4927d2f
Nico Peretti <85454960+Nicop9264@users.noreply.github.com>
2022-10-24 03:02:26 -0300

    Crud Categorias

---
+++ CrudCategorias/src/test/java/com/dh/CrudCategorias/CrudCategoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.CrudCategorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCategoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
