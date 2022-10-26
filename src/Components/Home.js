c4637dd287b71d045e6df7ec882e2b9f1b2a2e61
ayelenjaramillo <ayelenjaramillotw@gmail.com>
2022-10-25 22:00:08 -0300

    adelantos de frontend

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,29 @@
-import { Link } from "react-router-dom";
+import ReactDatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css"; 
+import "../Components/Home.css"; 
+import React, { useState} from 'react';
 
 const Home =()=>{
-    return
-    (
-        <h1> esta es la pagina principal del HOME</h1>
-    )
-}
+    const[date, setDate] = useState(new Date()); 
+    return(
+        <div className="h-principal">
+         <form> 
+            <h2>Busca ofertas en hoteles, casas y mucho mas</h2>
+                 <div className="h-sec">
+                    <select name="ciudad">
+                        <option>Chubut</option>
+                        <option>Cordoba</option>
+                        <option>Buenos Aires</option>
+                        <option>Neuquen</option>
+                    </select>
+                    <ReactDatePicker selected={date}  onChange={(date: Date)=>{
+                    setDate(date); 
+                    console.log(date); 
+                        }}/>
+                        <button type="submit">BUSCAR </button>
+                  </div>
+            </form>
+        </div>
+)
+   }; 
 export default Home; 
--- src/Components/Registro.js
+++ src/Components/Registro.js
@@ -1,8 +1,43 @@
 import { Link } from "react-router-dom";
+import "../Components/Registro.css"
 
 const Registro =()=>{
     return(
-        <h1> esta es la pagina principal del REGISTRO</h1>
-    )
+        <div>
+        
+           <div className="container_principal">
+            <form className="formulario">
+            <p className="text"> Crear cuenta</p>
+                <label> Nombre</label>
+                <br/>
+                <input type ="text"/>
+                <label> Apellido </label>
+                <input type="text"/>
+                <br/>
+                <label> Email</label>
+                <br/>
+                <input type="email"/>
+                <br/>
+                <label>Contraseña</label>
+                <br/>
+                <input type="password"/>
+                <br/>
+                <label> Confirmar Contraseña</label>
+                <br/>
+                <input type="password"/>
+                <br/>
+                <button className="boton_principal">
+                Crear Cuenta
+                </button>
+                <br/>
+                <p>Ya tenes cuenta?
+                <Link to="/LogIn" className="text">
+                    Iniciar sesion
+                </Link></p>
+                </form>
+                </div>
+                </div> 
+        
+    ); 
 }
 export default Registro; 
