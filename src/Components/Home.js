48a268c9c550a645c67c032cbf94c52da13d64d7
ayelenjaramillo <ayelenjaramillotw@gmail.com>
2022-10-26 21:58:12 -0300

    cambios de calendar y algo de responsive

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,29 +1,35 @@
-import ReactDatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css"; 
+import DatePicker from "react-datepicker";
 import "../Components/Home.css"; 
 import React, { useState} from 'react';
 
 const Home =()=>{
-    const[date, setDate] = useState(new Date()); 
-    return(
-        <div className="h-principal">
-         <form> 
-            <h2>Busca ofertas en hoteles, casas y mucho mas</h2>
-                 <div className="h-sec">
-                    <select name="ciudad">
-                        <option>Chubut</option>
-                        <option>Cordoba</option>
-                        <option>Buenos Aires</option>
-                        <option>Neuquen</option>
-                    </select>
-                    <ReactDatePicker selected={date}  onChange={(date: Date)=>{
-                    setDate(date); 
-                    console.log(date); 
-                        }}/>
-                        <button type="submit">BUSCAR </button>
-                  </div>
-            </form>
+const[startDate, setStartDate] = useState(); 
+const [endDate, setEndDate]= useState(); 
+function onChangeDateHandler(value){
+    setStartDate(value[0]); 
+    setEndDate(value[1]);
+    }
+return(
+<div className="h-principal">
+    <form> 
+        <h2>Busca ofertas en hoteles, casas y mucho mas</h2>
+        <div className="h-sec">
+            <select name="ciudad">
+                <option>Chubut</option>
+                <option>Cordoba</option>
+                <option>Buenos Aires</option>
+                <option>Neuquen</option>
+            </select>
+            <DatePicker selectsRange={true}
+                onChange = {onChangeDateHandler}
+                startDate={startDate}
+                endDate={endDate}
+                dateFormat="dd MM yyyy"/>
+            <button type="submit">BUSCAR </button>
         </div>
+    </form>
+</div>
 )
    }; 
 export default Home; 
