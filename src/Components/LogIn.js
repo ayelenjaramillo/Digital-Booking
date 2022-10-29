512adde057a9a3ebef6b08fe38ddc0ff06e36184
WinstonShaw <winstonsshaw@hotmail.com>
2022-10-29 02:14:51 -0300

    en LogIn.js agrego validaciones y comparacion con objeto prevLoginState

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -2,20 +2,34 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../Components/Login.css"; 
 const LogIn =()=>{
+
+    const prevLoginState = {
+        usuario:"usuario@hotmail.com", 
+        contrasenia: "123456"
+    }; 
     
     const[loginState, setLoginState] = useState({
         usuario:"", 
-        contrasenia: "", 
+        contrasenia: ""
     });     
+
+    const compararDatos = (event) => {
+        event.preventDefault();
+        console.log(loginState.contrasenia)
+        loginState.usuario === prevLoginState.usuario && loginState.contrasenia == prevLoginState.contrasenia ? window.location = "/MenuPrincLogin"
+         : alert("Por favor vuelva a intentarlo, sus credenciales son inválidas")
+        
+    }
+
     return(
     <div> 
         
         <div className="container_principal"> 
-  <form className="formulario">
+            <form className="formulario" onSubmit={compararDatos}>
                 <h3>Iniciar sesion</h3>
                 <label>Correo Electronico</label>
                 <br/>
-                <input type="email" placeholder="@..." onChange={(event)=>{
+                <input type="email" placeholder="@..." required pattern=".+@hotmail\.com" onChange={(event)=>{
                     const value = event.target.value; 
                     console.log(value);
                     setLoginState({...loginState, usuario: value})
@@ -23,24 +37,28 @@ const LogIn =()=>{
                 <br/>
                 <label>Contraseña</label>
                 <br/>
-                <input type="password" onChange={(event)=>{
+                <input type="password" minLength={6} required onChange={(event)=>{
                     const value = event.target.value; 
                     setLoginState({...loginState, contrasenia: value})}}/>
+                    
                 <br/>
-                <Link to="/MenuPrincLogin">
-                <button className="boton_principal" disabled ={!loginState.contrasenia || !loginState.usuario}>
-                INGRESAR     
+                {/* <Link to="/MenuPrincLogin">
+                    <button className="boton_principal" disabled ={!loginState.contrasenia || !loginState.usuario}>
+                    INGRESAR     
+                    </button>
+                </Link> */}
+                <button className="boton_principal">
+                    INGRESAR     
                 </button>
-                </Link>
                 <br/>
                 <p> Aun no tenes cuenta? 
                 <Link to="/Registro" className="text">
                   Registrarme
                 </Link>  </p>
-                </form>
+            </form>
         </div>
        
- </div>
+    </div>
     ); 
 }; 
 export default LogIn; 
