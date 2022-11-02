6e7477ab9276c474522102cddf62f26db7cb1e55
SUP <zb.silvina@gmail.com>
2022-11-02 17:21:04 -0300

    inicio hamburger menu

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,18 @@
 import {Link, Outlet} from "react-router-dom"; 
 import Componente from "../assets/Componente.png"; 
-import './Navbar.css'
+import './Navbar.css';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
 
 const NavBar =()=> {
     return(
 <>
 <header className="header-navbar">
   <h2 ><Link to="/Home"><img className="image" src={Componente} alt="title"/></Link></h2>  
-  <ul className="h-list">  
+  <ul className="h-list">    
+  <div className="hamburger-menu">
+  <GiHamburgerMenu />
+  </div>  
     <li><button><Link to = "/LogIn"> Iniciar Sesion </Link></button></li>
     <li><button><Link to = "/Registro"> Crear Cuenta </Link></button></li>
   </ul>
