4a327e0352ca497e9360e1ae0f96aded27905a19
Gaghyta <carla.antonini@hotmail.com>
2022-11-10 14:24:57 -0300

    Agrego bean eliminado accidentalmente

--- DigitalBooking/src/main/java/com/DH/DigitalBooking/services/PolicyService.java
+++ DigitalBooking/src/main/java/com/DH/DigitalBooking/services/PolicyService.java
@@ -3,10 +3,12 @@ import com.DH.DigitalBooking.exceptions.ResourceNotFoundException;
 import com.DH.DigitalBooking.models.Policy;
 import com.DH.DigitalBooking.repositories.PolicyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class PolicyService {
 
     @Autowired
