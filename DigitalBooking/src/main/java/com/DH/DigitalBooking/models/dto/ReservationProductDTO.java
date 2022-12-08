52fb4551c6f58cfd92b119bab59e27db8f08ef35
Gaghyta <carla.antonini@hotmail.com>
2022-12-08 03:12:06 -0300

    AGREGO DE UNA TODO DE FAVORITOS (Bookmarks) A codigo BACK Y también a BD (script e inserts)

--- DigitalBooking/src/main/java/com/DH/DigitalBooking/models/dto/ReservationProductDTO.java
+++ DigitalBooking/src/main/java/com/DH/DigitalBooking/models/dto/ReservationProductDTO.java
@@ -2,6 +2,8 @@ package com.DH.DigitalBooking.models.dto;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -13,6 +15,7 @@ import java.time.LocalTime;
 public class ReservationProductDTO {
         //CON ID
         private Long id;
+        @Temporal(TemporalType.DATE)
         @DateTimeFormat(pattern = "yyyy-MM-dd")
         private LocalDate checkIDate;
         @DateTimeFormat(pattern = "yyyy-MM-dd")
